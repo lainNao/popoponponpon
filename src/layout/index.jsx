876e696b2cb677e197ad3de8bf962d9646643cc9
aoao <nao.iwakura.lain.sheddy@gmail.com>
2020-09-05 19:57:51 +0900

    fix: 記事画面中央寄せ

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import config from "../../data/SiteConfig";
 import "./index.css";
+import 'twin.macro';
 
 export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <div className="layout-container">
+      <div>
         <Helmet>
           <meta name="description" content={config.siteDescription} />
           <html lang="ja" />
         </Helmet>
-        {children}
+        <div tw="flex justify-center">
+          <div tw="container p-4">
+            {children}
+          </div>
+        </div>
       </div>
     );
   }
